0aa5fcd378376cc8d3fc462af115bb2891ca5599
RBLAlexander <ralph@sapient.com.au>
2016-03-28 20:52:30 +1100

    Added new modelsfor trades offers and items

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,6 @@
 class Offer < ActiveRecord::Base
+	validates :description, presence: true, length: {in: 25..1000}
+	has_many :items
+
+	accepts_nested_attributes_for :items
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,7 @@
 class Trade < ActiveRecord::Base
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :description, presence: true, length: {in: 25..1000}
+	has_many :items
+
+	accepts_nested_attributes_for :items
 end
